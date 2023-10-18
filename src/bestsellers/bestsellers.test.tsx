d9fdaf05d0e43e0ffdf6b3009d24d9b433bf6658
tester <tester@test.com>
2023-10-18 14:24:58 +0200

    Update bestsellers.test.tsx update tests

--- src/bestsellers/bestsellers.test.tsx
+++ src/bestsellers/bestsellers.test.tsx
@@ -1,15 +1,16 @@
-import { getAllByTestId, render, screen } from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
+import {expect, jest, test} from '@jest/globals';
 import Bestsellers from "./bestsellers";
 
-jest.mock("./Carrousel", () => () => (
-  <div data-testing="carrousel">Carrousel</div>
-));
+jest.mock("../components/carrousel/carrousel.component", () => () => {
+  return <div data-testing="carrousel">Carrousel</div>;
+});
 
 describe("renders bestsellers", () => {
-  it("", () => {
-    const {getByTestId} = render(<Bestsellers />);
-    const carrouselElement = getAllByTestId("carrousel");
-    const gamesTableElement = getAllByTestId("games-table");
-    
+  test("Check for ids?", () => { // @note do we check ids?
+    render(<Bestsellers/>);
+
+    expect(screen.queryByTestId("carrousel")).toBeDefined()
+    expect(screen.queryByTestId("games-table")).toBeDefined()
   })
 });
